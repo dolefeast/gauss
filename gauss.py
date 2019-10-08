9f79dc8b3863fb558070e1b584769451e120d2bb
dolefeast <elrois456yt@gmail.com>
2019-10-08 12:24:55 +0200

    pequeños cambios

--- gauss.py
+++ gauss.py
@@ -1,5 +1,4 @@
 from sympy import *
-from numpy import sqrt
 init_printing()
 
 nvar = 1
@@ -9,7 +8,7 @@ variables = []
 
 var = input('Variable {}: '.format(nvar))
 while var != '':
-    dvar = float(input('Error de la variable {}: '.format(nvar)))
+    dvar = float(input('Error de la variable {}: '.format(var)))
     j = symbols(var)
     exec('%s = j' % var)
     variables.append(var)
@@ -18,9 +17,10 @@ while var != '':
     var = input('Variable {}: '.format(nvar))
 
 nvar -= 1
+
 g = symbols('g', cls = Function)
 
-g = eval(input('\nIntroduce relación entre dichas variables'))
+g = eval(input('\nIntroduce relación entre dichas variables: '))
 
 dgsquared = symbols('dg2')
 dgsquared = 0
@@ -28,10 +28,14 @@ dgsquared = 0
 for i in range(nvar):
     dgsquared += (diff(g, variables[i])*errores[i])**2
 
-print(dgsquared)
-if input('Quieres evaluar el error?[s/n] ') == 's':
-    valor_variables = ()
-    for i in range(nvar):
-        valor_variables += (float(input('Valor de la variable {}: '.format(variables[i]))), )
-    dg = lambdify(variables, dgsquared)
-    print(sqrt(dg(*valor_variables)))
+print('\n', dgsquared, '\n')
+valor_variables = ()
+
+
+
+
+for i in range(nvar):
+    valor_variables += (float(input('Valor de la variable {}: '.format(variables[i]))), )
+dg = lambdify(variables, dgsquared)
+
+print('El error de tu magnitud es:', sqrt(dg(*valor_variables)))
